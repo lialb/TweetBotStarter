9b502b9f05dac8882f23adeb39750e822ebc5d8b
ElegantDalek <karatedkid@gmail.com>
2019-04-03 19:08:26 -0500

    Added complete markov generator code

---
+++ markov_generator.py
@@ -0,0 +1,74 @@
+import random
+import pandas as pd
+import string
+import argparse
+
+TWEET_LENGTH = 280
+
+'''
+ Returns a dictionary with word frequencies associated with each word
+ @param library: an array of strings to parse from
+ @return a tuple containing beginning words, word-frequency dictionary
+'''
+def get_markov_dict(library):
+    word_chain = {}
+    begin_words = []
+    for sentence in library:
+        last_word = ''
+        clean_sentence = clean_string(sentence)
+        begin_words.append(clean_sentence.split(' ')[0])
+        for word in clean_sentence.split(' '):
+            if word not in word_chain:
+                word_chain[word] = []
+            if last_word != '':
+                word_chain[last_word].append(word)
+            last_word = word
+    return begin_words, word_chain
+
+'''
+Used to strip string of mentions, hashtags so we don't get banned.
+Also replaces html encoding of &amp with &
+@param input: the string to clean words from
+@return the cleaned string
+'''
+def clean_string(input):
+    input = input.replace('&amp', '&')
+    input = input.replace('&;', ';')
+    input = input.replace('@', '')
+    input = input.replace('#', '')
+    return input;
+
+'''
+Uses pandas to read the csv file for tweet text
+@param csv: the csv filename as a string
+@return a data frame object 
+'''
+def get_tweets_csv(csv):
+    df = pd.read_csv(csv)
+    return df['text'].tolist()
+
+'''
+Main function, takes in a csv file and generates tweet text based on that data
+@param csv: the csv file name to pull tweets from
+@return the generated markov chain string
+'''
+def generate_tweet(csv):
+    begin_words, word_chain = get_markov_dict(get_tweets_csv(csv))
+    word = random.choice(begin_words)
+    ret_str = ''
+    for i in range(100):
+        if (len(ret_str) + len(word + ' ')) > TWEET_LENGTH:
+            break
+        ret_str += word + ' '
+        clean_word = clean_string(word)
+        if (word_chain[clean_word] == []):
+            break
+        word = random.choice(word_chain[clean_word])
+    return ret_str
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('csv_file', type=str, help='the csv file to make tweets using')
+    args = parser.parse_args()
+    print(generate_tweet(args.csv_file))
+
