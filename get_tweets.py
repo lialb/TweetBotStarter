b56a96df0eca0358f3a9bf16ed6f1da8f26860a7
ElegantDalek <karatedkid@gmail.com>
2019-04-06 10:34:27 -0500

    Implement argparsing, more tutorial messaging

--- get_tweets.py
+++ get_tweets.py
@@ -4,6 +4,7 @@ from requests_oauthlib import OAuth1
 import json
 import csv
 import pprint
+import argparse
 from markov_generator import generate_tweet
 
 pp = pprint.PrettyPrinter()
@@ -12,7 +13,7 @@ pp = pprint.PrettyPrinter()
 After you have finished make_tweets, we will now retrieve tweets from people on
 Twitter. Firstly, try the authentication; it is similar to how we finished make_tweets
 '''
-with open() as :
+with open(API_KEY_FILE_HERE) as :
     api_keys = json.load()
 # Authentication for Twitter loaded into OAuth1 object
 #Your key, secret, access_token, and access_secret should go here
@@ -31,3 +32,17 @@ def get_tweets(tweet_handle, writeto):
         'exclude_replies': True,
         'trim_user': True,
     }
+
+    with open(WRITE_FILE_HERE) as :
+        fieldnames = ['id', 'text', 'source']
+        
+        # Now lets use csv.DictWriter to write this to a file!
+        # https://docs.python.org/3/library/csv.html
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('tweet_handle', type=str, help='the tweet handle to get tweets from, case insensitive and without the @')
+    parser.add_argument('write_to', type=str, help='what file to write the tweet data to')
+    args = parser.parse_args()
+    get_tweets(args.tweet_handle, args.write_to)
--- make_tweets.py
+++ make_tweets.py
@@ -6,18 +6,18 @@ from requests_oauthlib import OAuth1
 from markov_generator import generate_tweet
 
 '''
-This class will be used to make the tweets themselves. We will be using the tweepy and json library.
+This class will be used to make the tweets themselves. We will be using the requests and json library.
 This will make the actual tweets from the AI generated tweets. It parses through the json containing the keys and access tokens.
 '''
 
 def make_tweets(csv_file, num_tweets):
     with open('KEY FILE HERE') as :
         api_keys = 
-    auth = OAuth1()
+    auth = OAuth1(ADD AUTHENTICATION HERE)
 
     post_url = 'https://api.twitter.com/1.1/statuses/update.json'
     post_params = {
-        'status': generate_tweet(csv_file)
+        'status': TEXT_TO_TWEET_HERE
     }
 
     # TODO: After we get posting working, try tweeting num_tweets tweets every 10 seconds!
@@ -26,6 +26,7 @@ def make_tweets(csv_file, num_tweets):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+
     parser.add_argument('csv_file', type=str, help='the csv file to make tweets using')
     parser.add_argument('num_tweets', type=int, nargs='?', default=10, help='the number of tweets to make')
     args = parser.parse_args()
